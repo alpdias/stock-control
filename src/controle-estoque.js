063aa8373e014ba6f0b0eabcd57330079d4e8a43
Paulo <al.pdias24@gmail.com>
2020-01-09 22:09:08 -0300

    add js functions

--- src/controle-estoque.js
+++ src/controle-estoque.js
@@ -5,3 +5,19 @@ Criado em 01/2020
 
 var produto = document.getElementById("produto")
 var quantidade = document.getElementById("quantidade")
+var preço = document.getElementById("preço")
+
+function verificarEntrada() {
+    if (produto.value == "" || quantidade.value == 0 || preço.value <= 0) 
+    window.alert(`ERRO! Preencha todos os dados para inserir um produto no estoque.`)
+}
+
+function verificarPreçoSaida() {
+    if (preço.value > 0)
+        window.alert(`ERRO! Não preencha 'PREÇO' para remover um produto do estoque. `)
+}
+
+function verificarSaida() {
+    if (produto.value == "" || quantidade.value == 0) 
+    window.alert(`ERRO! Preencha 'PRODUTO' e 'QUANTIDADE' para remover um produto do estoque.`)
+}
