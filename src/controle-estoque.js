c9a9722798cc6f14e230f3f014d74ff1ce98806a
Paulo <al.pdias24@gmail.com>
2020-01-16 22:55:55 -0300

    add new functions

--- src/controle-estoque.js
+++ src/controle-estoque.js
@@ -3,21 +3,32 @@ Criado em 01/2020
 @Autor: Paulo https://github.com/alpdias
 */
 
-var produto = document.getElementById("produto")
-var quantidade = document.getElementById("quantidade")
-var preço = document.getElementById("preço")
+var produto = document.getElementById("produto") // variavel que recebe o input com id 'produto'
+var quantidade = document.getElementById("quantidade") // variavel que recebe o input com id 'quantidade'
+var preço = document.getElementById("preço") // variavel que recebe o input com id 'preço'
 
-function verificarEntrada() {
-    if (produto.value == "" || quantidade.value == 0 || preço.value <= 0) 
+function verificar() { // funçao para veriicar se todos os items estao preenchidos
+    if (produto.value == "" || quantidade.value == 0 || preço.value <= 0) {
     window.alert(`ERRO! Preencha todos os dados para inserir um produto no estoque.`)
+    } 
 }
 
-function verificarPreçoSaida() {
-    if (preço.value > 0)
-        window.alert(`ERRO! Não preencha 'PREÇO' para remover um produto do estoque. `)
-}
-
-function verificarSaida() {
-    if (produto.value == "" || quantidade.value == 0) 
+function saida() { // funçao para verificar a saida esta valida
+    if (preço.value > 0) {
+    window.alert(`ERRO! Não preencha 'PREÇO' para remover um produto do estoque.`)
+    } else if (produto.value == "" || quantidade.value == 0) {
     window.alert(`ERRO! Preencha 'PRODUTO' e 'QUANTIDADE' para remover um produto do estoque.`)
+    }
+}
+
+function adicionar() { // funcao para adicionar um produto ao estoque
+    var novo = document.getElementById("produto").value // recebe o 'valor' da variavel de um input
+    var qtd = document.getElementById("quantidade").value // recebe o 'valor' da variavel de um input
+    var prç = document.getElementById("preço").value // recebe o 'valor' da variavel de um input
+
+    item = { // cria um objeto
+        nome: novo, // atributos do objeto
+        quant: qtd,
+        valor: prç,
+    }
 }
