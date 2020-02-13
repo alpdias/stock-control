3637d2d2fdfa47202bb0b251222d07e4e3eae4c3
Paulo <al.pdias24@gmail.com>
2020-02-12 22:30:38 -0300

    organized JS

--- src/JavaScript/scripts.js
+++ src/JavaScript/scripts.js
@@ -7,33 +7,38 @@ var produto = document.getElementById("produto") // variavel que recebe o input 
 var quantidade = document.getElementById("quantidade") // variavel que recebe o input com id 'quantidade'
 var preço = document.getElementById("preço") // variavel que recebe o input com id 'preço'
 
-function verificar() { // funçao para verificar se todos os items estao preenchidos
+// funçao para verificar se todos os items estao preenchidos antes de adicionar
+function verificar() { 
     if (produto.value == "" || quantidade.value == 0 || preço.value <= 0) {
-    window.alert(`Preencha todos os campos para inserir um produto no estoque.`)
+    window.alert(`Preencha todos os campos para adicionar um produto no estoque.`)
     } 
 }
 
-function excluir() { // funçao para verificar a saida esta valida
+// funçao para excluir toda a lista de estoque
+function excluir() { 
     localStorage.removeItem("estoqueItens")
     window.alert(`Estoque excluído!!`)
 }
 
-function adicionar() { // funcao para adicionar um produto ao estoque
-    var novo = document.getElementById("produto").value // recebe o 'valor' da variavel de um input
-    var qtd = document.getElementById("quantidade").value // recebe o 'valor' da variavel de um input
-    var prç = document.getElementById("preço").value // recebe o 'valor' da variavel de um input
+// funcao para adicionar um produto ao estoque
+function adicionar() { 
+    var novo = document.getElementById("produto").value // recebe o 'valor' da variavel > produto
+    var qtd = document.getElementById("quantidade").value // recebe o 'valor' da variavel > quantidade
+    var prç = document.getElementById("preço").value // recebe o 'valor' da variavel > preço
 
-    item = { // cria um objeto
-        nome: novo, // atributos do objeto
+    // cria um objeto
+    item = { 
+        // atributos do objeto
+        nome: novo, 
         quant: qtd,
         valor: prç,
     }
 
-
-    if (localStorage.getItem('estoqueItens') === null) { // criar uma tabela para armazenar dados caso não exista
+    // criar uma tabela para armazenar os dados, caso não exista
+    if (localStorage.getItem('estoqueItens') === null) { 
         var itens = []
         itens.push(item)
-        localStorage.setItem('estoqueItens', JSON.stringify(itens)) // armazena um item no navegador e transfora em um JSON do tipo string 
+        localStorage.setItem('estoqueItens', JSON.stringify(itens)) // armazena um item no navegador e transforma em um JSON do tipo string 
     } else {
         var itens = JSON.parse(localStorage.getItem('estoqueItens')) // transforma em um JSON do tipo objeto
         itens.push(item)
@@ -41,19 +46,27 @@ function adicionar() { // funcao para adicionar um produto ao estoque
     }
 }
 
-function mostrarResultado() { // funcao para mostrar os itens adicionados
+// funcao para mostrar os itens adicionados
+function mostrarResultado() { 
     var itens = JSON.parse(localStorage.getItem('estoqueItens')) // recebe as informaçoes armazenadas, passando para um JSON em objeto
     var resultadoItens = document.getElementById('resultados') // busca o elemento HTML pelo ID
 
     resultadoItens.innerHTML = ''
 
-    for (var i = 0; i < itens.length; i++) { // loop para mostrar os resultados buscando dentro do array
+    // loop para mostrar os resultados buscando dentro do array
+    for (var i = 0; i < itens.length; i++) { 
         var nome =  itens[i].nome
         var quant = itens[i].quant
         var valor = itens[i].valor
-            
-        resultadoItens.innerHTML += '<tr><td>' + nome + '</td><td>' + quant + '</td><td>' + valor + '</td><td>' + '<button style="background-color: red; font-family: Arial, Helvetica, sans-serif; color: white; padding: 2%; border-radius: 4px; margin-top: 3%; margin-bottom: 0px; border: transparent; font-weight: bolder;">&nbsp;&nbsp;X&nbsp;&nbsp;</button>' + '</td></tr>' // exibi o resultado 
+        
+        // insere os resultado dentro da tabela em HTML pelo JS
+        resultadoItens.innerHTML += '<tr><td>' + nome + 
+                                    '</td><td>' + quant + 
+                                    '</td><td>' + valor + 
+                                    '</td><td>' + '<button style="background-color: red; font-family: Arial, Helvetica, sans-serif; color: white; padding: 2%; border-radius: 4px; margin-top: 3%; margin-bottom: 0px; border: transparent; font-weight: bolder;">&nbsp;&nbsp;X&nbsp;&nbsp;</button>' + 
+                                    '</td></tr>'
 
+        // zera os campos apos inserir os dados
         produto.value = ''
         quantidade.value = ''
         preço.value = ''
